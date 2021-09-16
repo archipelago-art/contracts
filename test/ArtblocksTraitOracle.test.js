9d227bf417b72c8e825d912d1239ab793fc9e510
William Chargin <wchargin@gmail.com>
2021-09-16 13:15:24 -0700

    Add tests for trait ID computation

--- test/ArtblocksTraitOracle.test.js
+++ test/ArtblocksTraitOracle.test.js
@@ -1,6 +1,27 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+const TraitType = Object.freeze({
+  PROJECT: 0,
+  FEATURE: 1,
+});
+
+function projectTraitId(projectId, version) {
+  const blob = ethers.utils.defaultAbiCoder.encode(
+    ["uint256", "uint256", "uint256"],
+    [TraitType.PROJECT, projectId, version]
+  );
+  return ethers.utils.keccak256(blob);
+}
+
+function featureTraitId(projectId, featureName, version) {
+  const blob = ethers.utils.defaultAbiCoder.encode(
+    ["uint256", "uint256", "string", "uint256"],
+    [TraitType.FEATURE, projectId, featureName, version]
+  );
+  return ethers.utils.keccak256(blob);
+}
+
 describe("ArtblocksTraitOracle", () => {
   let ArtblocksTraitOracle;
   before(async () => {
@@ -12,5 +33,25 @@ describe("ArtblocksTraitOracle", () => {
   it("deploys", async () => {
     const oracle = await ArtblocksTraitOracle.deploy();
     await oracle.deployed();
+  });
+
+  describe("computes trait IDs", () => {
+    let oracle;
+    before(async () => {
+      oracle = await ArtblocksTraitOracle.deploy();
+      await oracle.deployed();
+    });
+
+    it("for projects", async () => {
+      expect(await oracle.projectTraitId(23, 0)).to.equal(
+        projectTraitId(23, 0)
+      );
+    });
+
+    it("for features", async () => {
+      expect(await oracle.featureTraitId(23, "Palette: Paddle", 0)).to.equal(
+        featureTraitId(23, "Palette: Paddle", 0)
+      );
+    });
   });
 });
