e48ced133580add3ad7502f2fc57d79df7c310ea
Dandelion Mané <decentralion@dandelion.io>
2021-09-16 14:25:32 -0700

    Incomplete but tested Market implementation
    Has working signatures :)
    
    Paired with the inestimable @wchargin

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
 enum BidType {
     /// A bid for a specific token, keyed by token ID.
     SINGLE_TOKEN,
@@ -59,33 +63,109 @@ enum CancellationType {
     ASKS_ALL,
     /// Matches asks with a specific nonce.
     ASKS_BY_NONCE,
-    /// Matches asks on a specific token ID.
-    ASKS_BY_TOKEN_ID,
     /// Matches all bids. The associated `parameter` must be zero.
     BIDS_ALL,
     /// Matches bids with a specific nonce.
-    BIDS_BY_NONCE,
-    /// Matches bids that specifically target a given token ID. Does not match
-    /// bids on a traitset that the given token happens to match.
-    BIDS_BY_TOKEN_ID,
-    /// Matches bids on a given traitset. The traitset is keyed by the
-    /// `keccak256` hash of the ABI encoding of the sorted `uint256[]` array of
-    /// traits. This only matches bids with *exactly* the specified traitset.
-    BIDS_BY_TRAITSET
+    BIDS_BY_NONCE
 }
 
 contract Market {
-    mapping(address => uint256) public cancellationTimeAsksAll;
-    mapping(address => mapping(uint256 => uint256))
-        public cancellationTimeAsksByNonce;
-    mapping(address => mapping(uint256 => uint256))
-        public cancellationTimeAsksByTokenId;
+    IERC721 token;
+    IERC20 weth;
+    mapping(address => uint256) public bidTimestampCancellation;
+    mapping(address => mapping(uint256 => bool)) public bidNonceCancellation;
+    mapping(address => uint256) public askTimestampCancellation;
+    mapping(address => mapping(uint256 => bool)) public askNonceCancellation;
 
-    mapping(address => uint256) public cancellationTimeBidsAll;
-    mapping(address => mapping(uint256 => uint256))
-        public cancellationTimeBidsByNonce;
-    mapping(address => mapping(uint256 => uint256))
-        public cancellationTimeBidsByTokenId;
-    mapping(address => mapping(uint256 => uint256))
-        public cancellationTimeBidsByTraitset;
+    function initialize(IERC721 _token, IERC20 _weth) external {
+        require(
+            address(token) == address(0) && address(weth) == address(0),
+            "already initialized"
+        );
+        token = _token;
+        weth = _weth;
+    }
+
+    function _verify(bytes memory _message, bytes memory _signature)
+        internal
+        pure
+        returns (address)
+    {
+        bytes32 _rawHash = keccak256(_message);
+        bytes32 _ethMessageHash = ECDSA.toEthSignedMessageHash(_rawHash);
+        return ECDSA.recover(_ethMessageHash, _signature);
+    }
+
+    function fillOrder(
+        Bid memory bid,
+        bytes memory bidSignature,
+        Ask memory ask,
+        bytes memory askSignature
+    ) external {
+        bytes memory bidMessage = abi.encode(bid);
+        bytes memory askMessage = abi.encode(ask);
+
+        address bidder = _verify(bidMessage, bidSignature);
+        address asker = _verify(askMessage, askSignature);
+
+        _fillOrder(bid, bidder, ask, asker);
+    }
+
+    function _fillOrder(
+        Bid memory bid,
+        address bidder,
+        Ask memory ask,
+        address asker
+    ) internal {
+        bool ownerOrApproved;
+        uint256 tokenId = ask.tokenId;
+        address tokenOwner = token.ownerOf(tokenId);
+        if (tokenOwner == asker) {
+            ownerOrApproved = true;
+        }
+        if (token.getApproved(tokenId) == asker) {
+            ownerOrApproved = true;
+        }
+        if (token.isApprovedForAll(tokenOwner, asker)) {
+            ownerOrApproved = true;
+        }
+        require(ownerOrApproved, "asker is not owner or approved");
+
+        require(block.timestamp <= bid.deadline, "bid expired");
+        require(block.timestamp <= ask.deadline, "ask expired");
+
+        require(
+            bidTimestampCancellation[bidder] < bid.created,
+            "bid cancelled (timestamp)"
+        );
+        require(
+            !bidNonceCancellation[bidder][bid.nonce],
+            "bid cancelled (nonce)"
+        );
+        require(
+            askTimestampCancellation[asker] < ask.created,
+            "ask cancelled (timestamp)"
+        );
+        require(
+            !askNonceCancellation[asker][ask.nonce],
+            "ask cancelled (nonce)"
+        );
+
+        require(bid.price == ask.price, "price mismatch");
+
+        if (bid.bidType == BidType.SINGLE_TOKEN) {
+            require(bid.tokenId == ask.tokenId, "tokenid mismatch");
+        } else {
+            revert("not yet supported");
+            // TODO: Consult trait oracle and verify tokenid is included in trait.
+        }
+
+        token.safeTransferFrom(asker, bidder, tokenId);
+        weth.transferFrom(bidder, asker, bid.price);
+        // TODO: royalties
+
+        // bids and asks are cancelled on execution, to prevent replays
+        bidNonceCancellation[bidder][bid.nonce] = true;
+        askNonceCancellation[asker][ask.nonce] = true;
+    }
 }
--- test/Market.test.js
+++ test/Market.test.js
@@ -1,14 +1,247 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+const BN = ethers.BigNumber;
+
+const BidType = Object.freeze({
+  SINGLE_TOKEN: 0,
+  TRAITSET: 1,
+});
+
 describe("Market", () => {
+  const exa = BN.from("10").pow(18);
   let Market;
+  let TestERC20;
+  let TestERC721;
   before(async () => {
-    Market = await ethers.getContractFactory("Market");
+    [Market, TestERC20, TestERC721] = await Promise.all([
+      ethers.getContractFactory("Market"),
+      ethers.getContractFactory("TestERC20"),
+      ethers.getContractFactory("TestERC721"),
+    ]);
   });
 
+  async function setup() {
+    const signers = await ethers.getSigners();
+    const [market, weth, nft] = await Promise.all([
+      Market.deploy(),
+      TestERC20.deploy(),
+      TestERC721.deploy(),
+    ]);
+    await Promise.all([market.deployed(), weth.deployed(), nft.deployed()]);
+    await market.initialize(nft.address, weth.address);
+    const bidder = signers[1];
+    const asker = signers[2];
+    await weth.mint(bidder.address, exa); // give bidder 1 full weth
+    await weth
+      .connect(bidder)
+      .approve(market.address, ethers.constants.MaxUint256);
+    // give asker token ids 0 and 1
+    await nft.mint(asker.address, 0);
+    await nft.mint(asker.address, 1);
+    await nft.connect(asker).setApprovalForAll(market.address, true);
+    return { signers, market, weth, nft, bidder, asker };
+  }
+
   it("deploys", async () => {
-    const market = await Market.deploy();
-    await market.deployed();
+    const { market } = await setup();
+  });
+
+  function tokenIdBid({
+    nonce = 0,
+    created = 1,
+    deadline = ethers.constants.MaxUint256,
+    price = exa,
+    tokenId = 0,
+  } = {}) {
+    return {
+      nonce,
+      created,
+      deadline,
+      price,
+      tokenId,
+      traitset: [],
+      bidType: BidType.SINGLE_TOKEN,
+    };
+  }
+
+  function traitsetBid({
+    nonce = 0,
+    created = 1,
+    deadline = ethers.constants.MaxUint256,
+    price = exa,
+    traitset = [],
+  } = {}) {
+    return {
+      nonce,
+      created,
+      deadline,
+      price,
+      tokenId: 0,
+      traitset,
+      bidType: BidType.TRAITSET,
+    };
+  }
+
+  function newAsk({
+    nonce = 0,
+    created = 1,
+    deadline = ethers.constants.MaxUint256,
+    price = exa,
+    tokenId = 0,
+  } = {}) {
+    return {
+      nonce,
+      created,
+      deadline,
+      price,
+      tokenId,
+    };
+  }
+
+  async function signBlob(blob, signer) {
+    const hash = ethers.utils.arrayify(ethers.utils.keccak256(blob));
+    const result = await signer.signMessage(hash);
+    return result;
+  }
+
+  async function signBid(bid, signer) {
+    const blob = ethers.utils.defaultAbiCoder.encode(
+      [
+        {
+          components: [
+            {
+              internalType: "uint256",
+              name: "nonce",
+              type: "uint256",
+            },
+            {
+              internalType: "uint256",
+              name: "created",
+              type: "uint256",
+            },
+            {
+              internalType: "uint256",
+              name: "deadline",
+              type: "uint256",
+            },
+            {
+              internalType: "uint256",
+              name: "price",
+              type: "uint256",
+            },
+            {
+              internalType: "enum BidType",
+              name: "bidType",
+              type: "uint8",
+            },
+            {
+              internalType: "uint256",
+              name: "tokenId",
+              type: "uint256",
+            },
+            {
+              internalType: "uint256[]",
+              name: "traitset",
+              type: "uint256[]",
+            },
+          ],
+          internalType: "struct Bid",
+          name: "bid",
+          type: "tuple",
+        },
+      ],
+      [
+        [
+          bid.nonce,
+          bid.created,
+          bid.deadline,
+          bid.price,
+          bid.bidType,
+          bid.tokenId,
+          bid.traitset,
+        ],
+      ]
+    );
+    return signBlob(blob, signer);
+  }
+
+  async function signAsk(ask, signer) {
+    const blob = ethers.utils.defaultAbiCoder.encode(
+      [
+        {
+          components: [
+            {
+              internalType: "uint256",
+              name: "nonce",
+              type: "uint256",
+            },
+            {
+              internalType: "uint256",
+              name: "created",
+              type: "uint256",
+            },
+            {
+              internalType: "uint256",
+              name: "deadline",
+              type: "uint256",
+            },
+            {
+              internalType: "uint256",
+              name: "price",
+              type: "uint256",
+            },
+            {
+              internalType: "uint256",
+              name: "tokenId",
+              type: "uint256",
+            },
+          ],
+          internalType: "struct Ask",
+          name: "ask",
+          type: "tuple",
+        },
+      ],
+      [[ask.nonce, ask.created, ask.deadline, ask.price, ask.tokenId]]
+    );
+    return signBlob(blob, signer);
+  }
+
+  async function fillOrder(market, bid, bidder, ask, asker) {
+    const bidSignature = await signBid(bid, bidder);
+    const askSignature = await signAsk(ask, asker);
+    return market.fillOrder(bid, bidSignature, ask, askSignature);
+  }
+
+  describe("order filling", () => {
+    it("works in a basic case", async () => {
+      const { market, signers, weth, nft, asker, bidder } = await setup();
+      expect(await nft.ownerOf(0)).to.equal(asker.address);
+      const bid = tokenIdBid();
+      const ask = newAsk();
+
+      await fillOrder(market, bid, bidder, ask, asker);
+      expect(await nft.ownerOf(0)).to.equal(bidder.address);
+      expect(await weth.balanceOf(bidder.address)).to.equal(0);
+      expect(await weth.balanceOf(asker.address)).to.equal(exa); // TODO: fix when we add royalties
+    });
+
+    describe.skip("token approvals", () => {
+      it("rejects if asker lacks approvals", async () => {
+        const { market, signers, weth, nft, bidder } = await setup();
+        const bid = tokenIdBid();
+        const ask = newAsk();
+        // tbc
+      });
+      it("works if asker is owner", async () => {
+        //
+      });
+      it("works if asker is approved for all", async () => {
+        //
+      });
+      it("works if asker has token approval", async () => {
+        //
+      });
+    });
   });
 });
