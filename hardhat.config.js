04de9e80b0e05581d6f3bbf5207ef3712f188f8f
Dandelion Mané <decentralion@dandelion.io>
2021-11-24 13:28:53 -0800

    Upgrade to solidity 0.8.9
    It allows some type conversions that we want.

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,7 +19,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: {
-    version: "0.8.4",
+    version: "0.8.9",
     settings: {
       optimizer: {
         enabled: true,
